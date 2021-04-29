734e26bf714f2d55f182939be6b4bf4b601c7c00
William Guevara <will.dev@hotmail.com>
2021-04-29 11:27:27 -0500

    feat: add filters hotels by name, country, stars

--- api/src/routes/hotels.route.ts
+++ api/src/routes/hotels.route.ts
@@ -23,7 +23,7 @@ route
 	.get('/id/:id', (req: Request, res: Response, next: NextFunction) => {
 		const { id } = req.params;
 		const logger: Logger = Container.get('logger');
-		logger.debug(`Calling GET to /hotels/:${id} endpoint`);
+		logger.debug(`Calling GET to /hotels/${id} endpoint`);
 
 		try {
 			const buffer: ListHotels = JSON.parse(readJson(jsonPath));
@@ -35,11 +35,25 @@ route
 			next(error);
 		}
 	})
-	.get('/name/:name', (req: Request, res: Response, next: NextFunction) => {})
+	.get('/name/:name', (req: Request, res: Response, next: NextFunction) => {
+		const { name } = req.params;
+		const logger: Logger = Container.get('logger');
+		logger.debug(`Calling GET to /hotels/${name} endpoint`);
+
+		try {
+			const buffer: ListHotels = JSON.parse(readJson(jsonPath));
+			const hotelServices = Container.get(HotelService);
+			const filtered = hotelServices.filterBy('name', name, buffer);
+
+			res.json(filtered).status(200);
+		} catch (error) {
+			next(error);
+		}
+	})
 	.get('/city/:city', (req: Request, res: Response, next: NextFunction) => {
 		const { city } = req.params;
 		const logger: Logger = Container.get('logger');
-		logger.debug(`Calling GET to /hotels/:${city} endpoint`);
+		logger.debug(`Calling GET to /hotels/${city} endpoint`);
 
 		try {
 			const buffer: ListHotels = JSON.parse(readJson(jsonPath));
@@ -51,7 +65,35 @@ route
 			next(error);
 		}
 	})
-	.get('/country/:country', (req: Request, res: Response, next: NextFunction) => {})
-	.get('/stars/:stars', (req: Request, res: Response, next: NextFunction) => {});
+	.get('/country/:country', (req: Request, res: Response, next: NextFunction) => {
+		const { country } = req.params;
+		const logger: Logger = Container.get('logger');
+		logger.debug(`Calling GET to /hotels/:${country} endpoint`);
+
+		try {
+			const buffer: ListHotels = JSON.parse(readJson(jsonPath));
+			const hotelServices = Container.get(HotelService);
+			const filtered = hotelServices.filterBy('country', country, buffer);
+
+			res.json(filtered).status(200);
+		} catch (error) {
+			next(error);
+		}
+	})
+	.get('/stars/:stars', (req: Request, res: Response, next: NextFunction) => {
+		const { stars } = req.params;
+		const logger: Logger = Container.get('logger');
+		logger.debug(`Calling GET to /hotels/:${stars} endpoint`);
+
+		try {
+			const buffer: ListHotels = JSON.parse(readJson(jsonPath));
+			const hotelServices = Container.get(HotelService);
+			const filtered = hotelServices.filterBy('stars', stars, buffer);
+
+			res.json(filtered).status(200);
+		} catch (error) {
+			next(error);
+		}
+	});
 
 export default route;
