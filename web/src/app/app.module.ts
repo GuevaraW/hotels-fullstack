23ffe0c03c7c2879db83cbb88f03dc3f304461de
William Guevara <will.dev@hotmail.com>
2021-04-29 14:14:18 -0500

    feat(web): add service to execute requests to the api

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HotelsListComponent } from './components/hotels-list/hotels-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HotelsListComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
