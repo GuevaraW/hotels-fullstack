7cc2f07429bab6ec0e3a7d468373ded9cfa36032
William Guevara <will.dev@hotmail.com>
2021-04-29 19:13:07 -0500

    feat(api): add normalization to filter parameters in hotel service

--- api/src/services/hotels.service.ts
+++ api/src/services/hotels.service.ts
@@ -1,18 +1,37 @@
-import { Hotel } from 'src/types';
-import { Service, Container } from 'typedi';
-import { Logger } from 'winston';
+import { Hotel, ListHotels } from 'src/types';
+import { Service } from 'typedi';
 import { ErrorHandler } from '../helpers/ErrorHandler';
 
 @Service()
 export default class HotelService {
 	constructor() {}
 
-	filterBy(key: string, value: unknown, repo: Array<Hotel>) {
-		const valueSanit = typeof value === 'string' ? value.replace('+', ' ') : value;
-		const filtered = repo.filter((hotel) => {
-			return hotel[key] === valueSanit;
+	filterBy(key: string, value: string, repo: ListHotels): ListHotels {
+		const filtered = repo.filter((hotel: Hotel) => {
+			let field = hotel[key];
+			if (typeof field === 'string') {
+				return this.findString(field, value);
+			}
+			if (typeof field === 'number') {
+				return field.toString() === value;
+			}
 		});
-		if (filtered[key]) return filtered;
+
+		if (filtered.length) return filtered;
 		throw new ErrorHandler(404, 'Not Found');
+	}
+
+	private findString(string: string, searchString: string): boolean {
+		searchString = this.normalizeString(searchString);
+		const str = this.normalizeString(string);
+		return str.includes(searchString);
+	}
+
+	private normalizeString(string: string) {
+		return string
+			.toLowerCase()
+			.normalize('NFD')
+			.replace(/[\u0300-\u036f]/g, '')
+			.replace('+', ' ');
 	}
 }
