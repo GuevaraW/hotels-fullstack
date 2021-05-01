ffc3496211806b7cfddabd543350c569d68343c7
William Guevara <will.dev@hotmail.com>
2021-04-30 23:25:33 -0500

    feat(web): add page hotel details

--- web/src/app/components/hotel-details/hotel-details.component.ts
+++ web/src/app/components/hotel-details/hotel-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListHotels, Hotel } from 'src/app/models/Hotel';
+import { HotelService } from 'src/app/services/hotel.service';
 
 @Component({
   selector: 'app-hotel-details',
   templateUrl: './hotel-details.component.html',
-  styleUrls: ['./hotel-details.component.scss']
+  styleUrls: ['./hotel-details.component.scss'],
 })
 export class HotelDetailsComponent implements OnInit {
+  hotel!: Hotel;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    public hotelService: HotelService
+  ) {}
 
   ngOnInit(): void {
+    this.getHotelDetails();
   }
 
+  getHotelDetails() {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.hotelService.getHotelById(id).subscribe((data: ListHotels) => {
+      this.hotel = data[0];
+    });
+  }
 }
