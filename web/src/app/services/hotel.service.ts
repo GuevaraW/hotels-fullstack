745b5fa948a05a005edc2358b6a0eef91238cf78
William Guevara <will.dev@hotmail.com>
2021-04-30 21:38:58 -0500

    feat(web): add handling errors to api calls and redirect to error page

--- web/src/app/services/hotel.service.ts
+++ web/src/app/services/hotel.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { ListHotels } from '../models/Hotel';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HotelService {
-  constructor(private http: HttpClient) {}
-  getHotels(path: string): Observable<any> {
-    return this.http.get(path);
+  constructor(private router: Router, private http: HttpClient) {}
+
+  getHotels(path: string): Observable<ListHotels> {
+    return this.http.get<ListHotels>(path);
+  }
+
+  getHotelById(id: string): Observable<ListHotels> {
+    return this.http
+      .get<ListHotels>('http://localhost:3000/api/hotels/id/' + id)
+      .pipe(catchError((error) => this.handleErrors(error)));
+  }
+
+  handleErrors(err: HttpErrorResponse): Observable<any> {
+    if (err.status === 404) {
+      this.router.navigate(['/not-found']);
+    } else {
+      this.router.navigate(['/hotels']);
+    }
+    return of(err.message);
   }
 }
